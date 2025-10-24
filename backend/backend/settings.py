ab9d12fd701f8b984522214697ac06c584bef94d
YasmimRap0S0 <yasmim.fraposo@gmail.com>
2025-10-24 16:17:22 -0300

    PT3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -115,19 +115,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sisteminha',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost', # IP_WSL: localhost | 172.18.176.1
-        'PORT': '5432',
-    },
-    
-     'sqlite': {  # SQLite
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
